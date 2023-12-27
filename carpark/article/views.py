91d24e8303aa226ba884a4a5a87c705e5469e8ce
raduscortescu <radusco01@gmail.com>
2023-12-27 16:31:13 +0200

    Added Pagination

--- carpark/article/views.py
+++ carpark/article/views.py
@@ -7,6 +7,8 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
+import math
 # Create your views here.
 
 class ArticleView(generics.CreateAPIView):
@@ -43,16 +45,32 @@ class ArticleView(generics.CreateAPIView):
 
         return Response("Article created successfully", status=status.HTTP_200_OK)
 
+class CustomPagination(PageNumberPagination):
+    page_size = 1 # default page size
+    max_page_size = 1000 # default max page size
+    page_size_query_param = 'page_size' # if you want to dynamic items per page from request you must have to add it 
+      
+    def get_paginated_response(self, data):
+        # if you want to show page size in resposne just add these 2 lines
+        if self.request.query_params.get('page_size'):
+            self.page_size = int(self.request.query_params.get('page_size'))
+            
+        # you can count total page from request by total and page_size
+        total_page = math.ceil(self.page.paginator.count / self.page_size)
+        
+        # here is your response
+        return Response({
+            'count': self.page.paginator.count,
+            'total': total_page,
+            'page_size': self.page_size,
+            'current': self.page.number,
+            'previous': self.get_previous_link(),
+            'next': self.get_next_link(),
+            'results': data
+        })
+
 # GET all Articles
 class ArticleListView(generics.ListAPIView):
-    queryset = Article.objects.all()
+    queryset = Article.objects.filter(is_featured=True)
     serializer_class = ArticlesListSerializer
-
-    search_fields = ['title']
-    ordering_fields = ['timestamp']
-    pagination_class = PageNumberPagination
-
-    def get_queryset(self):
-        # Retrieve only featured articles
-        queryset = Article.objects.filter(is_featured=True)
-        return queryset
+    pagination_class = CustomPagination
