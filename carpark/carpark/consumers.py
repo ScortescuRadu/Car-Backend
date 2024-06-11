0a9ee8ee6c448eb65f0531ab7e8b2d223e3bc5ef
raduscortescu <radusco01@gmail.com>
2024-06-11 10:59:31 +0300

    migrated license plate detection to socket

--- carpark/carpark/consumers.py
+++ carpark/carpark/consumers.py
@@ -3,6 +3,22 @@ import json
 import base64
 from django.core.files.base import ContentFile
 from .utils import process_image_and_extract_license_plate
+from user_park.models import UserPark
+from parking_invoice.models import ParkingInvoice
+from user_profile.models import UserProfile
+from ultralytics import YOLO
+import os
+import cv2
+import numpy as np
+from PIL import Image, ImageEnhance
+import logging
+from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
+from io import BytesIO
+from easyocr import Reader
+from datetime import timedelta
+from django.utils import timezone
+from asgiref.sync import sync_to_async
 
 class TaskStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -97,3 +113,121 @@ class CameraUpdateConsumer(AsyncWebsocketConsumer):
 
             # Log the result or perform any additional operations here
             print(f'Received frame from camera: {camera_address}, license plate: {license_plate}, destination: {destination_type}')
+
+
+model_license_plate = YOLO('/Users/raduscortescu/Desktop/Car-Backend/carpark/image_task/license_plate_detector.pt')
+reader = Reader(['en'])
+
+class EntranceExitFrameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add("entrance_exit_frame", self.channel_name)
+        await self.accept()
+        print("WebSocket connection opened")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("entrance_exit_frame", self.channel_name)
+        print(f"WebSocket connection closed with code: {close_code}")
+
+    async def receive(self, text_data):
+        print("Message received from WebSocket")
+        try:
+            data = json.loads(text_data)
+            image_data = base64.b64decode(data['image'])
+            camera_address = data['device_id_0']
+            parking_lot_address = data['parking_lot']
+            token = data['token']
+
+            image_np = self.convert_image_data_to_np(image_data)
+
+            results = model_license_plate(image_np)
+            ocr_texts = self.process_detections(results, image_np, token, parking_lot_address)
+
+            response_data = {
+                'camera_address': camera_address,
+                'parking_lot': parking_lot_address,
+                'ocr_texts': ocr_texts
+            }
+
+            await self.send(text_data=json.dumps(response_data))
+            print(f"Sent response data: {response_data}")
+
+        except Exception as e:
+            print(f"Error processing WebSocket message: {str(e)}")
+
+    def convert_image_data_to_np(self, image_data):
+        import cv2
+        image = Image.open(BytesIO(image_data))
+        image = image.convert('RGB')
+        image_np = np.array(image)
+        image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+        return image_np
+
+    def process_detections(self, results, image_np, token, parking_lot_address):
+        ocr_texts = []
+        for result in results:
+            for box in result.boxes:
+                bbox = box.xyxy[0].cpu().numpy().tolist()
+                conf = float(box.conf.cpu().numpy())
+                cls = int(box.cls.cpu().numpy())
+                class_name = model_license_plate.names[cls] if cls in model_license_plate.names else 'unknown'
+
+                if class_name == "license_plate":
+                    license_plate_img = image_np[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+                    ocr_text = self.perform_ocr(license_plate_img)
+                    ocr_texts.append(ocr_text)
+
+                    self.create_invoice_if_needed(ocr_text, token, parking_lot_address)
+
+        return ocr_texts
+
+    def perform_ocr(self, license_plate_img):
+        try:
+            ocr_results = reader.readtext(license_plate_img)
+            if ocr_results:
+                print(f"OCR results: {ocr_results}")
+                return ocr_results[0][1]
+        except Exception as e:
+            print(f"Error during OCR: {str(e)}")
+        return ''
+
+    @sync_to_async
+    def create_invoice_if_needed(self, ocr_text, token, parking_lot_address):
+        from django.conf import settings
+
+        try:
+            if ocr_text:
+                recent_time = timezone.now() - timedelta(minutes=2)
+                existing_invoices = ParkingInvoice.objects.filter(
+                    license_plate=ocr_text,
+                    timestamp__gte=recent_time
+                )
+
+                if not existing_invoices.exists():
+                    user = None
+                    if token:
+                        try:
+                            user = Token.objects.get(key=token).user
+                        except Token.DoesNotExist:
+                            print("Token does not exist")
+
+                    if not user:
+                        try:
+                            user_profile = UserProfile.objects.get(car_id=ocr_text)
+                            user = user_profile.user
+                        except UserProfile.DoesNotExist:
+                            user = settings.AUTH_USER_MODEL.objects.first()  # Default user
+
+                    parking_lot = get_object_or_404(ParkingLot, street_address=parking_lot_address)
+                    ParkingInvoice.objects.create(
+                        user=user,
+                        parking_lot=parking_lot,
+                        hourly_price=parking_lot.price,
+                        spot_description='Example spot',
+                        time_spent=1,
+                        final_cost=parking_lot.price,
+                        license_plate=ocr_text
+                    )
+                    print(f"Created new invoice for license plate: {ocr_text}")
+
+        except Exception as e:
+            print(f"Error creating invoice: {str(e)}")
--- carpark/carpark/routing.py
+++ carpark/carpark/routing.py
@@ -3,11 +3,13 @@ from .consumers import (
     TaskStatusConsumer,
     ParkingLotUpdateDataConsumer,
     ParkingSpotUpdateConsumer,
-    CameraUpdateConsumer)
+    CameraUpdateConsumer,
+    EntranceExitFrameConsumer)
 
 websocket_urlpatterns = [
     re_path(r'^ws/task_status/(?P<task_id>[0-9a-f-]+)/$', TaskStatusConsumer.as_asgi()),
     re_path(r'ws/parking_lot_updates/$', ParkingLotUpdateDataConsumer.as_asgi()),
     re_path(r'ws/parking_spot_updates/$', ParkingSpotUpdateConsumer.as_asgi()),
     re_path(r'^ws/camera_updates/$', CameraUpdateConsumer.as_asgi()),
+    re_path(r'^ws/entrance_exit_frames/$', EntranceExitFrameConsumer.as_asgi()),
 ]
